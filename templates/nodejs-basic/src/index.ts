d5cdfc88d0b53d91f356720363ab5a886b1accb8
Othon Ferreira <othondev@gmail.com>
2020-11-12 20:01:21 +1000

    Added necessary middlewares

--- templates/nodejs-basic/src/index.ts
+++ templates/nodejs-basic/src/index.ts
@@ -1,18 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { stream } from "config/winston";
-import info from "core/routes/info";
-import { clientErrorHandler, errorHandler, logErrors } from "core/handlers/error"
+import infoRoute from "core/routes/info";
+import {
+  clientErrorHandler,
+  errorHandler,
+  logErrors,
+} from "core/handlers/error";
 
 const app = express();
 
 app.use(morgan("combined", { stream }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(info);
+app.use(infoRoute);
 
-app.use(logErrors)
-app.use(clientErrorHandler)
-app.use(errorHandler)
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 
 export default app;
